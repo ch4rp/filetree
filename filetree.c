574732f859648c672e6230530e0f483358256148
ch4rp <ahmetchusrev1234@gmail.com>
2023-02-12 17:18:02 +0300

    add filetree.c

---
+++ filetree.c
@@ -0,0 +1,91 @@
+/*
+ * Title   : filetree (for unix)
+ * Date    : 12 February 2023
+ * Author  : ch4rp
+ * 
+ * Example usage:
+ * main.c:
+		#include <stdio.h>
+		int tree(const char *path, void (*func)(const char *, int));
+		
+		void printfile(const char *string, int filetype)
+		{
+			printf("%c %s\n", filetype ? 'D' : '-', string);
+		}
+		
+		int main(int argc, char **argv)
+		{
+			void (*ret)(const char *, int) = printfile;
+			tree(argv[1], ret);
+			return 0;
+		} 
+  $ gcc -c main.c
+  $ gcc -c filetree.c
+  $ gcc main.o filetree.o -o tree
+  $ ./tree /home
+ */
+
+
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define MAX_PATH_LENG	1024
+
+
+int tree(const char *path, void (*func)(const char *, int))
+{
+	// directory = 1
+	// not directory = 0
+	static enum {KFILE = 0, KDIR = 1};
+	
+	DIR *currdir;
+	struct dirent *direnv;
+	struct stat    envinf;
+	char filepath[MAX_PATH_LENG];
+	int  filetype;
+	
+	if ((currdir = opendir(path)) == NULL)
+		return 1;
+
+	while (errno = 0, (direnv = readdir(currdir)) != NULL) {
+		filetype = KFILE;
+		
+		// skip directories '.' and '..'
+		if (!strcmp(direnv->d_name, ".") || !strcmp(direnv->d_name, ".."))
+			continue;
+
+		// fill the array with '\0' characters for new file path
+		memset(filepath, 0, MAX_PATH_LENG);
+		// copy path to array
+		strcpy(filepath, path);
+		// just in case 
+		strcat(filepath, "/");
+		// add the file name to the end of array
+		strcat(filepath, direnv->d_name);
+
+		if (stat(filepath, &envinf) < 0) 
+			continue;
+		
+		// if file is a directory
+		if (envinf.st_mode & S_IFDIR) {
+			// apply same operations to file if file is a directory
+			tree(filepath, func);
+			filetype = KDIR;
+		}
+		
+		(*func)(filepath, filetype);
+	}
+
+	if (errno != 0)
+		return errno;
+
+	closedir(currdir);
+
+	return 0;
+}
+
+
